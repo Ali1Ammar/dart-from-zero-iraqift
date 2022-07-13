757e5b24cb6bc1fdf576bb148f7af6d016373ce5
Ali <aliammarali125@gmail.com>
2022-07-13 14:12:28 +0300

    edit loop

--- 9- loop/main.dart
+++ 9- loop/main.dart
@@ -1,7 +1,7 @@
 void main(List<String> args) {
   int num1 = 10;
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; i=i*5) {
     print(i);
   }
 
@@ -23,6 +23,27 @@ void main(List<String> args) {
     print(num1);
     num1++;
   }
+
+  while ((num1*3) < 10) {
+    print(num1);
+    num1++;
+  }
+
+    while ((num1*3).isOdd) {
+    print(num1);
+    num1++;
+  }
+
+    for (int i = 5+1; i.compareTo(10)==0; i*=2) {
+    if (i == 5) {
+      break;
+    }
+    print(i);
+  }
+
+  
+
+
 
   do {
     print(num1);
